4ff683493aebd1d6fb9539617a73b61b9a9ebb47
lamnguyenvu98 <lamnguyenvu98@gmail.com>
2024-07-14 05:16:54 +0700

    update

--- yowo/cli.py
+++ yowo/cli.py
@@ -2,7 +2,6 @@ from lightning.pytorch.cli import (
     LightningArgumentParser, 
     LightningCLI
 )
-import torch
 
 from yowo.models import YOWOv2Lightning
 from yowo.data import *
@@ -22,14 +21,12 @@ default_optimizer = {
         "fused": None
     },
 }
-
 default_scheduler = {
     "class_path": "torch.optim.lr_scheduler.MultiStepLR",
     "init_args": {
         "milestones": [2, 4, 5],
         "gamma": 0.1,
-        "last_epoch": -1,
-        "verbose": False if torch.__version__ <= '2.1.2' else "deprecated"
+        "last_epoch": -1
     },
 }
 
@@ -39,15 +36,6 @@ default_scheduler_config = {
 }
 
 class YowoLightningCLI(LightningCLI):
-    def before_instantiate_classes(self) -> None:
-        subcommand = self.config["subcommand"]
-        scheduler_ns =  self.config[subcommand]["model"]["scheduler"]
-        # same older version <= torch 2.1.2, verbose is boolean
-        # latter version verbose="deprecated", and will be removed
-        # in upcomming versions
-        if "verbose" in scheduler_ns['init_args'].keys():
-            scheduler_ns["init_args"].pop("verbose")
-    
     def add_arguments_to_parser(self, parser: LightningArgumentParser) -> None:
         parser.set_defaults({
             "model.optimizer": default_optimizer,
@@ -62,6 +50,7 @@ class YowoLightningCLI(LightningCLI):
 def main():
     cli = YowoLightningCLI(
         model_class=YOWOv2Lightning,
+        save_config_kwargs={"overwrite": True},
     )
 
 if __name__ == '__main__':
