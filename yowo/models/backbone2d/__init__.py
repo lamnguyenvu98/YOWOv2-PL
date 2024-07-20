a39749567536ec7728b46be82c3c62b9e8b5f4d3
lamnguyenvu98 <lamnguyenvu98@gmail.com>
2024-07-20 07:22:55 +0700

    update

--- yowo/models/backbone2d/__init__.py
+++ yowo/models/backbone2d/__init__.py
@@ -8,5 +8,5 @@ def build_backbone_2d(model_name: str, pretrained: bool = False, use_blurpool: b
     )
     if use_blurpool:
         from .blurpool2d import apply_blurpool
-        apply_blurpool(model, replace_conv=False)
+        apply_blurpool(model)
     return model, feat
